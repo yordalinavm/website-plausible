355dc13be38e1ef413a1b2f678fe88d386bc7ca7
yordalinavm <yordalina.metodieva@insitex.com>
2024-04-25 12:40:25 +0300

    Updates

---
+++ component---src-pages-dogs-page-js-09b862be0e15f8a22707.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{Wwq7:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),l=t.n(a);n.default=e=>{let{dogs:n}=e;return l.a.createElement(l.a.Fragment,null,l.a.createElement("h1",null,"Dogs"),l.a.createElement("p",null,n))}}}]);
+//# sourceMappingURL=component---src-pages-dogs-page-js-09b862be0e15f8a22707.js.map
